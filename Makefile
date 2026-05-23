e72c706cacee656bfb4f8428f821dd6991607ad6
denivladislav <deni.vladislav@gmail.com>
2026-05-23 18:02:48 +0300

    step5: tests, fixture and coverage added

--- Makefile
+++ Makefile
@@ -17,4 +17,7 @@ lint-fix:
 test:
 	go test ./...
 
-.PHONY: build run lint-install lint fmt lint-fix test
+test-coverage:
+	go test ./... -coverpkg=./... -coverprofile=coverage.out -covermode=atomic
+
+.PHONY: build run lint-install lint fmt lint-fix test test-coverage
---
+++ gendiff_test.go
@@ -0,0 +1,109 @@
+package code
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"code/internal/format"
+	"code/internal/parse"
+)
+
+var testDataPath = filepath.Join(".", "testdata", "fixture")
+
+func validateError(t *testing.T, err error, checkErr func(err error) bool) {
+	t.Helper()
+
+	if !checkErr(err) {
+		t.Errorf("error check failed for error: %v", err)
+	}
+}
+
+func TestGenDiff_Errors(t *testing.T) {
+	tests := map[string]struct {
+		pathA, pathB, formatName string
+		checkErr                 func(err error) bool
+	}{
+		"unreachable path causes error": {
+			pathA:      "",
+			pathB:      filepath.Join(testDataPath, "file2.json"),
+			formatName: "stylish",
+			checkErr: func(err error) bool {
+				return errors.Is(err, os.ErrNotExist)
+			},
+		},
+		"path to directory causes error": {
+			pathA:      testDataPath,
+			pathB:      filepath.Join(testDataPath, "file2.json"),
+			formatName: "stylish",
+			checkErr: func(err error) bool {
+				var pathErr *fs.PathError
+				return errors.As(err, &pathErr)
+			},
+		},
+		"unsupported file extension causes error": {
+			pathA:      filepath.Join(testDataPath, "unsupported.diff"),
+			pathB:      filepath.Join(testDataPath, "file2.json"),
+			formatName: "stylish",
+			checkErr: func(err error) bool {
+				var extErr parse.UnsupportedExtError
+				return errors.As(err, &extErr)
+			},
+		},
+		"invalid file causes error": {
+			pathA:      filepath.Join(testDataPath, "invalid.json"),
+			pathB:      filepath.Join(testDataPath, "file2.json"),
+			formatName: "stylish",
+			checkErr: func(err error) bool {
+				return err != nil
+			},
+		},
+		"unsupported output format causes error": {
+			pathA:      filepath.Join(testDataPath, "file1.json"),
+			pathB:      filepath.Join(testDataPath, "file2.json"),
+			formatName: "unknown",
+			checkErr: func(err error) bool {
+				var formatError format.UnsupportedFormatError
+				return errors.As(err, &formatError)
+			},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			_, err := GenDiff(tt.pathA, tt.pathB, tt.formatName)
+
+			validateError(t, err, tt.checkErr)
+		})
+	}
+}
+
+func TestGenDiff(t *testing.T) {
+	tests := map[string]struct {
+		pathA, pathB, formatName, pathWant string
+	}{
+		"generates correct diff for json with stylish format": {
+			pathA:      filepath.Join(testDataPath, "file1.json"),
+			pathB:      filepath.Join(testDataPath, "file2.json"),
+			formatName: "stylish",
+			pathWant:   filepath.Join(testDataPath, "result_json_stylish.txt"),
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			bytesWant, err := os.ReadFile(tt.pathWant)
+			require.NoError(t, err)
+
+			got, err := GenDiff(tt.pathA, tt.pathB, tt.formatName)
+			require.NoError(t, err)
+
+			assert.Equal(t, string(bytesWant), got)
+		})
+	}
+}

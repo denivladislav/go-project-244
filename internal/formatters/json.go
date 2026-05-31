4f30be713942b8129fad244871fa607a9ef8b591
denivladislav <deni.vladislav@gmail.com>
2026-05-31 11:16:18 +0300

    fix: json format updated

--- internal/formatters/json.go
+++ internal/formatters/json.go
@@ -7,10 +7,20 @@ import (
 	"code/internal/ast"
 )
 
+type Root struct {
+	Key      string  `json:"key"`
+	Children ast.AST `json:"children"`
+}
+
 func MakeJson(nodes ast.AST) (string, error) {
 	indent := strings.Repeat(" ", 2)
 
-	b, err := json.MarshalIndent(nodes, "", indent)
+	root := Root{
+		Key:      "root",
+		Children: nodes,
+	}
+
+	b, err := json.MarshalIndent(root, "", indent)
 	if err != nil {
 		return "", err
 	}
